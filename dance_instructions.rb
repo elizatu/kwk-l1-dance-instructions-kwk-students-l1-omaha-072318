35d4a03570a3f54702bf95ecfdda40adf09961c7
elizatu <eliza.turco@gmail.com>
2018-07-24 20:59:31 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,16 @@
 
 # Code your methods below
+def starting_stance
+  puts "Plant legs far apart"
+end
+
+def skip_step(left)
+  puts "Lower"
+  puts "Bounce"
+  puts "Briefly"
+end
+
+def base_footwork(right)
+  puts "Lift and return foot"
+end
+  
---
+++ dogs.rb
@@ -0,0 +1,6 @@
+
+adjective = "awesome"
+first = "Eliza"
+last = "Turco"
+age = "14"
+puts " Hi my name is #{first} #{last} and I am #{age}."
